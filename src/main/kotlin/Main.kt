958cbcc21d89aec0fbe866d92f39c03766cf7737
Walaa9999 <walaa.dweikat@harri.com>
2022-02-02 13:33:49 +0200

    create mult function with default values

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,12 @@ package basic.syntax
 fun main() {
     println("Walaa")
     println(sum(7,8));
+    println(mult());
 }
 fun sum(a:Int, b:Int):Int{
     return a+b;
+}
+
+fun mult(a:Int =10, b:Int=5):Int{
+    return a*b;
 }
