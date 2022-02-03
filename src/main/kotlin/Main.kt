28a82ad07d9370d2eb7ee90d6616e6f878155bee
Walaa9999 <walaa.dweikat@harri.com>
2022-02-03 10:06:41 +0200

    creating intefers and floatingpoints functions in BasicTypes file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,14 @@
 package basic.syntax
+import concepts.types.*
 fun main() {
     println("Walaa")
-    println(sum(7,8));
-    println(mult());
-}
-fun sum(a:Int, b:Int):Int{
-    return a+b;
-}
 
-fun mult(a:Int =10, b:Int=5):Int{
-    return a*b;
+    types();
+    val i = 1
+    val d = 1.0
+    val f = 1.0f
+    printDouble(d)
+    //printDouble(i) // Error: Type mismatch
+    //printDouble(f) // Error: Type mismatch
+
 }
---
+++ src/main/kotlin/concepts/types/BasicTypes.kt
@@ -0,0 +1,43 @@
+package concepts.types
+
+fun integers() {
+    //Integers
+    val one = 1 // Int
+    print(one);
+    val threeBillion = 3000000000 // Long
+    print(threeBillion);
+    val oneLong = 1L // Long
+    print(oneLong);
+    val oneByte: Byte = 1
+    println(oneByte);
+}
+
+fun floatingPoints(){
+    //Floating points
+    val pi = 3.14 // Double
+    // val one: Double = 1 // Error: type mismatch
+    val oneDouble = 1.0 // Double
+    val e = 2.7182818284 // Double
+    val eFloat = 2.7182818284f// Float, actual value is 2.7182817
+    println(eFloat);
+}
+
+
+fun printDouble(d: Double) { print(d) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
